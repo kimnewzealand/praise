819252ce9101bd8e6d11814c91f51066608267f0
KimF <kimfitter@yahoo.com>
2017-10-25 17:22:40 +1300

    added an example

--- R/praise.R
+++ R/praise.R
@@ -2,6 +2,10 @@
 #'
 #' @description Praise me at critical times in my life
 #'
+#' @examples
+#' # I'm going to deliver some praise
+#' praise()
+#'
 #' @export
 praise <- function() {
       print("You're the best!")
