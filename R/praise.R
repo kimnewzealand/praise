f82e06c1d0c26b91de31ecb523a7005e64ce96f4
KimF <kimfitter@yahoo.com>
2017-10-25 17:33:22 +1300

    Added name argument to praise function so we can praise others

--- R/praise.R
+++ R/praise.R
@@ -1,12 +1,14 @@
 #' @title Send some praise
 #'
-#' @description Praise me at critical times in my life
+#' @description Praise someone at critical times in their life
+#'
+#' @param name character, the name of the person you want to praise
 #'
 #' @examples
 #' # I'm going to deliver some praise
 #' praise()
 #'
 #' @export
-praise <- function() {
-      print("You're the best!")
+praise <- function(name = "Kim") {
+      paste("You're the best, ",name)
 }
